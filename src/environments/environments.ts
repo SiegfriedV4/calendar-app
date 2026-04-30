6f7e7b1a1ff86a6091949000895186ed987c06c9
Sashen <SGovindasamy@dvtsoftware.com>
2026-04-30 09:13:44 +0200

    fix: use dot notation fo env var

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  holidayApiKey: (import.meta as any).env?.['NG_APP_HOLIDAY_API_KEY'] ?? '',
+  holidayApiKey: (import.meta as any).env?.NG_APP_HOLIDAY_API_KEY ?? '',
   holidayApiCountry: 'ZA',
 };
